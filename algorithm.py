4d65808017c41553b007730b186407cdf816d7a6
Diana Clarissa Nansubuga <117372394+Diananansubuga@users.noreply.github.com>
2023-10-12 09:56:05 +0300

    Add files via upload

---
+++ algorithm.py
@@ -0,0 +1,9 @@
+# def five_present(arr):
+#     i=0;
+#     for i<arr.length; i++:
+#     if arr[i] == 5:
+#         return True;        
+#     return False;
+# arr=[1,4,8,7,5,3]
+# print(five_present([1,2])) #False
+
